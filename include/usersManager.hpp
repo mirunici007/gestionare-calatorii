ca0d284d43f1acb9005bfc4340ba1f3604e5e545
mirunici007 <mirunasardaru15@gmail.com>
2025-12-01 21:23:40 +0200

    first try for user activity

---
+++ include/usersManager.hpp
@@ -0,0 +1,37 @@
+#ifndef USERSMANAGER_HPP
+#define USERSMANAGER_HPP
+
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <vector>
+#include <random>
+#include <ctime>
+#include <sstream>
+#include <stdexcept>
+#include <algorithm>
+
+#include "user.hpp"
+
+
+class UsersManager{
+    std::vector<User> users;
+    static std::string generateID();
+    public:
+    UsersManager()=default;
+    ~UsersManager(){};
+
+    bool isEmailValid(const std::string &email)const;
+    void createUserAccount(const std::string &lastName, const std::string &firstName, const std::string &email, const std::string &password);
+
+    bool login(const std::string &email, const std::string &password)const;
+    void deleteUser(const std::string &email);
+    void updatePassword(const std::string &email, const std::string &newPassword);
+
+    void addUserToCSV(const std::string &filename, const User &user);
+    void loadUsersFromCSV(const std::string &filename);
+
+
+};
+
+#endif
